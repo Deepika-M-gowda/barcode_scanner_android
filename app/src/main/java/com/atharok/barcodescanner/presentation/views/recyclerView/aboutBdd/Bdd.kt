7472c61f318b9b7df59aef3fec9521231a3d6b14
Atharok <qfievet@protonmail.com>
2023-01-11 19:29:13 +0100

    Changed the color of the shortcuts icons. Added the new OpenFoodFacts logo.

--- app/src/main/java/com/atharok/barcodescanner/presentation/views/recyclerView/aboutBdd/Bdd.kt
+++ app/src/main/java/com/atharok/barcodescanner/presentation/views/recyclerView/aboutBdd/Bdd.kt
@@ -23,8 +23,8 @@ package com.atharok.barcodescanner.presentation.views.recyclerView.aboutBdd
 import com.atharok.barcodescanner.R
 
 enum class Bdd(val nameResource: Int, val descriptionResource: Int, val webLinkResource: Int, val drawableResource: Int) {
-    OPEN_FOOD_FACTS(R.string.open_food_facts_label, R.string.bdd_open_food_facts_description_label, R.string.search_engine_open_food_facts_url, R.drawable.open_food_facts_logo_en_356x300),
+    OPEN_FOOD_FACTS(R.string.open_food_facts_label, R.string.bdd_open_food_facts_description_label, R.string.search_engine_open_food_facts_url, R.drawable.open_food_facts_logo),
     OPEN_PET_FOOD_FACTS(R.string.open_pet_food_facts_label, R.string.bdd_open_pet_food_facts_description_label, R.string.search_engine_open_pet_food_facts_url, R.drawable.open_pet_food_facts_logo_en_356x300),
     OPEN_BEAUTY_FACTS(R.string.open_beauty_facts_label, R.string.bdd_open_beauty_facts_description_label, R.string.search_engine_open_beauty_facts_url, R.drawable.open_beauty_facts_logo_en_356x300),
-    OPEN_LIBRARY_FACTS(R.string.open_library_label, R.string.bdd_open_library_description_label, R.string.search_engine_open_library_url, R.drawable.open_library_logo_356x87)
+    OPEN_LIBRARY_FACTS(R.string.open_library_label, R.string.bdd_open_library_description_label, R.string.search_engine_open_library_url, R.drawable.open_library_logo)
 }
