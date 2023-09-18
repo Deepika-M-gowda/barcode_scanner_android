0ff3346f0db153f3f7e1cc0ee8c6c7484c8473b9
Hu Shenghao <dede.hu@qq.com>
2023-09-18 07:54:01 +0000

    Provides backward compatibility for language change.

--- app/src/main/java/com/atharok/barcodescanner/presentation/views/fragments/main/MainSettingsFragment.kt
+++ app/src/main/java/com/atharok/barcodescanner/presentation/views/fragments/main/MainSettingsFragment.kt
@@ -21,20 +21,30 @@
 package com.atharok.barcodescanner.presentation.views.fragments.main
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import android.provider.Settings
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.atharok.barcodescanner.R
 import com.atharok.barcodescanner.presentation.intent.createSearchUrlIntent
 import com.atharok.barcodescanner.presentation.intent.createStartActivityIntent
 import com.atharok.barcodescanner.presentation.views.activities.*
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.util.Locale
 import kotlin.reflect.KClass
 
 class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
@@ -42,7 +52,7 @@ class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSha
     override fun onAttach(context: Context) {
         super.onAttach(context)
         val activity: Activity = requireActivity()
-        if(activity is BaseActivity){
+        if (activity is BaseActivity) {
             activity.lockDeviceRotation(false)
         }
     }
@@ -50,7 +60,7 @@ class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSha
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, null)
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             configureChangeLanguagePreference()
         }
 
@@ -74,8 +84,8 @@ class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSha
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         val mActivity = requireActivity()
-        if(mActivity is MainActivity){
-            when(key){
+        if (mActivity is MainActivity) {
+            when (key) {
                 getString(R.string.preferences_color_key) -> mActivity.updateTheme()
                 getString(R.string.preferences_theme_key) -> mActivity.updateTheme()
 
@@ -95,13 +105,65 @@ class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSha
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun configureChangeLanguagePreference(){
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun configureChangeLanguagePreference() {
         val pref = findPreference(getString(R.string.preferences_languages_key)) as Preference?
 
-        if(pref != null) {
+        if (pref != null) {
+            val localList = arrayListOf(
+                R.string.preferences_default to LocaleListCompat.getEmptyLocaleList(),
+                R.string.locale_language_de to LocaleListCompat.create(Locale.GERMAN),
+                R.string.locale_language_en to LocaleListCompat.create(Locale.ENGLISH),
+                R.string.locale_language_es to LocaleListCompat.forLanguageTags("es"),
+                R.string.locale_language_fr to LocaleListCompat.create(Locale.FRENCH),
+                R.string.locale_language_in to LocaleListCompat.forLanguageTags("in"),
+                R.string.locale_language_it to LocaleListCompat.create(Locale.ITALIAN),
+                R.string.locale_language_nb_NO to LocaleListCompat.forLanguageTags("nb-NO"),
+                R.string.locale_language_pl to LocaleListCompat.forLanguageTags("pl"),
+                R.string.locale_language_pt_BR to LocaleListCompat.forLanguageTags("pt-BR"),
+                R.string.locale_language_ru to LocaleListCompat.forLanguageTags("ru"),
+                R.string.locale_language_tr to LocaleListCompat.forLanguageTags("tr"),
+                R.string.locale_language_uk to LocaleListCompat.forLanguageTags("uk"),
+                R.string.locale_language_zh to LocaleListCompat.create(Locale.SIMPLIFIED_CHINESE),
+                R.string.locale_language_zh_Tw to LocaleListCompat.create(Locale.TRADITIONAL_CHINESE),
+                R.string.locale_language_ja to LocaleListCompat.create(Locale.JAPANESE),
+            )
+            val currentLocales = AppCompatDelegate.getApplicationLocales()
+            var selectedIndex = localList.indexOfFirst { it.second == currentLocales }
+            if (selectedIndex != -1) {
+                pref.setSummary(localList[selectedIndex].first)
+            }
             pref.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-                startActivity(createActionAppLocaleSettingsIntent())
+                val items = localList.map { requireContext().getString(it.first) }.toTypedArray()
+                val savedIndex = selectedIndex
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.preferences_languages_change)
+                    .setSingleChoiceItems(items, selectedIndex) { dialogInterface, index ->
+                        selectedIndex = index
+                        val locales = localList[index].second
+                        val base = requireContext()
+                        val newConfig = Configuration(base.resources.configuration)
+                        newConfig.setLocales(locales.unwrap() as LocaleList?)
+                        val localeContext = base.createConfigurationContext(newConfig)
+                        val dialog = dialogInterface as Dialog
+                        items[0] = localeContext.getString(R.string.preferences_default)
+                        val adapter = dialog.findViewById<ListView>(R.id.select_dialog_listview)?.adapter
+                        (adapter as? BaseAdapter)?.notifyDataSetChanged()
+                        dialog.setTitle(localeContext.getString(R.string.preferences_languages_change))
+                        dialog.findViewById<TextView>(android.R.id.button1)
+                            ?.text = localeContext.getString(android.R.string.ok)
+                        dialog.findViewById<TextView>(android.R.id.button2)
+                            ?.text = localeContext.getString(android.R.string.cancel)
+                    }
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        if (selectedIndex != savedIndex) {
+                            val locales = localList[selectedIndex].second
+                            AppCompatDelegate.setApplicationLocales(locales)
+                        }
+                    }
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .show()
+                //startActivity(createActionAppLocaleSettingsIntent())
                 true
             }
         }
@@ -113,20 +175,20 @@ class MainSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSha
         Uri.fromParts("package", requireActivity().packageName, null)
     )
 
-    private fun configureAboutPreference(keyResource: Int, activityKClass: KClass<out Activity>){
+    private fun configureAboutPreference(keyResource: Int, activityKClass: KClass<out Activity>) {
         val pref = findPreference(getString(keyResource)) as Preference?
 
-        if(pref != null) {
+        if (pref != null) {
             pref.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 startAboutActivity(activityKClass)
             }
         }
     }
 
-    private fun configureSourceCodePreference(){
+    private fun configureSourceCodePreference() {
         val pref = findPreference(getString(R.string.preferences_source_code_key)) as Preference?
 
-        if(pref != null) {
+        if (pref != null) {
             pref.onPreferenceClickListener = Preference.OnPreferenceClickListener {
                 val url = requireActivity().getString(R.string.source_code_link)
                 val intent: Intent = createSearchUrlIntent(url)
