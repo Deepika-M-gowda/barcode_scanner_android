202dd6c61e7fb19fad4ef363c0542ba9c9c656d2
Atharok <atharok@duck.com>
2024-08-19 17:18:38 +0200

    Updated OFF, OBF and OPFF logos.

--- app/src/main/java/com/atharok/barcodescanner/presentation/views/recyclerView/aboutBdd/Bdd.kt
+++ app/src/main/java/com/atharok/barcodescanner/presentation/views/recyclerView/aboutBdd/Bdd.kt
@@ -24,8 +24,8 @@ import com.atharok.barcodescanner.R
 
 enum class Bdd(val nameResource: Int, val descriptionResource: Int, val webLinkResource: Int, val drawableResource: Int) {
     OPEN_FOOD_FACTS(R.string.open_food_facts_label, R.string.bdd_open_food_facts_description_label, R.string.search_engine_open_food_facts_url, R.drawable.open_food_facts_logo),
-    OPEN_PET_FOOD_FACTS(R.string.open_pet_food_facts_label, R.string.bdd_open_pet_food_facts_description_label, R.string.search_engine_open_pet_food_facts_url, R.drawable.open_pet_food_facts_logo_en_356x300),
-    OPEN_BEAUTY_FACTS(R.string.open_beauty_facts_label, R.string.bdd_open_beauty_facts_description_label, R.string.search_engine_open_beauty_facts_url, R.drawable.open_beauty_facts_logo_en_356x300),
+    OPEN_PET_FOOD_FACTS(R.string.open_pet_food_facts_label, R.string.bdd_open_pet_food_facts_description_label, R.string.search_engine_open_pet_food_facts_url, R.drawable.open_pet_food_facts_logo),
+    OPEN_BEAUTY_FACTS(R.string.open_beauty_facts_label, R.string.bdd_open_beauty_facts_description_label, R.string.search_engine_open_beauty_facts_url, R.drawable.open_beauty_facts_logo),
     MUSIC_BRAINZ(R.string.musicbrainz_label, R.string.bdd_music_brainz_description_label, R.string.search_engine_music_brainz_url, R.drawable.music_brainz_logo),
     OPEN_LIBRARY(R.string.open_library_label, R.string.bdd_open_library_description_label, R.string.search_engine_open_library_url, R.drawable.open_library_logo)
 }
