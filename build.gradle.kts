fa65cddfd6f9b41f0bdfc0185213941304de51ba
Deepika M <96332752+Deepika-M-gowda@users.noreply.github.com>
2025-01-22 15:40:32 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.devtools.ksp) apply false
+    id "org.sonarqube" version "6.0.1.5171"
 }
