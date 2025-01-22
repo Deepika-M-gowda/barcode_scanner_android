899d6b5cf153b5a4e945596b2ee9623404003e7d
Deepika M <96332752+Deepika-M-gowda@users.noreply.github.com>
2025-01-22 15:46:04 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'org.sonarqube' version '6.0.1.5171'
+    id("org.sonarqube") version "6.0.1.5171"
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.devtools.ksp) apply false
